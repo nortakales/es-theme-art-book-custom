e00ee70c5901c06334de7ac62ab2a37993ccc734
Nick Ortakales <nortakales@gmail.com>
2022-11-15 22:46:15 -0800

    Show game time, improve screenshots view

--- .vscode/copy-to-device.sh
+++ .vscode/copy-to-device.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 
-HOST=192.168.1.50
+HOST=192.168.1.99
 ssh root@$HOST mkdir -p /etc/emulationstation/themes/es-theme-art-book-custom && rm -rf /etc/emulationstation/themes/es-theme-art-book-custom/*
 scp -r ./* root@$HOST:/etc/emulationstation/themes/es-theme-art-book-custom/
